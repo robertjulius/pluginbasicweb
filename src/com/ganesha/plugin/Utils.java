ec31ede7de4b100c83b3d561d73b4a8280a77a4d
robert.julius.ext <robert.julius.ext@W7-IDJKT0089.wincor-nixdorf.com>
2013-03-19 14:16:29 +0700

    Mabok dah

--- src/com/ganesha/plugin/Utils.java
+++ src/com/ganesha/plugin/Utils.java
@@ -1,6 +1,8 @@
 package com.ganesha.plugin;
 
 import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FilenameFilter;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IFolder;
@@ -34,6 +36,41 @@ public class Utils {
 			((IProject) resource).create(monitor);
 			((IProject) resource).open(monitor);
 			break;
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		File file = new File(Utils.class.getClassLoader().getResource("")
+				.toURI());
+
+		file = file.getParentFile().listFiles(new FilenameFilter() {
+			@Override
+			public boolean accept(File dir, String name) {
+				if (name.equals("templates")) {
+					return true;
+				} else {
+					return false;
+				}
+			}
+		})[0];
+
+		scanFilesRecursively(file);
+	}
+
+	public static void scanFilesRecursively(File file) {
+		File[] childs = file.listFiles();
+		if (childs != null && childs.length > 0) {
+			for (File child : childs) {
+				scanFilesRecursively(child);
+			}
+		} else {
+			if (file.isFile()) {
+				System.out.println("files.add(\"" + file.getPath() + "\");");
+			} else {
+				/*
+				 * TODO
+				 */
+			}
 		}
 	}
 }
